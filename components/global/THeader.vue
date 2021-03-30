46de85b8f4cf462e772f7e5a8aeeed911dfd95fb
devbaraus <me@baraus.dev>
2021-03-30 19:32:40 -0300

    Remoção de seções da home

--- components/global/THeader.vue
+++ components/global/THeader.vue
@@ -1,15 +1,20 @@
 <template>
   <header class='header-section'>
     <div class='container'>
-      <div>
-        <img src='/img/logo_ifg.svg' style='height: 5.4rem;' class='pb-3' alt='Instituto Federal de Goiás'>
-        <a class='site-logo text-dark font-weight-bold d-none d-md-inline-block ml-md-3' style='font-size: 2rem;' href='index-2.html'>
-          Inteligencia <span style='color: var(--orange);'>Artificial</span>
-        </a>
-      </div>
+      <a class='site-logo text-dark font-weight-bold' href='index-2.html'>
+        <img src='/img/logo_ifg.svg' style='height: 4rem;' alt='Instituto Federal de Goiás'>
+      </a>
 
       <div class='nav-switch'>
         <i class='fa fa-bars'></i>
+      </div>
+      <div class='header-info d-flex align-items-center' style='height: 5rem;'>
+        <div class='hf-item'>
+          <a class='text-dark font-weight-bold align-baseline' style='font-size: 2rem;'
+             href='index-2.html'>
+            Inteligencia <span style='color: var(--orange);'>Artificial Aplicada</span>
+          </a>
+        </div>
       </div>
       <!--      <div class='header-info'>-->
       <!--        <div class='hf-item'>-->
--- components/home-sections/hero.vue
+++ components/home-sections/hero.vue
@@ -1,48 +1,36 @@
 <template>
   <section class="hero-section">
-    <client-only>
-      <carousel v-bind="{
-    loop: true,
-    nav: false,
-    dots: true,
-    mouseDrag: false,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    items: 1,
-    autoplay: true
-  }" class="hero-slider">
-        <div class="hs-item" style="background-image: url(/img/hero-slider/1.jpg);">
-          <div class="hs-text">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="hs-subtitle">Award Winning UNIVERSITY</div>
-                  <h2 class="hs-title">An investment in knowledge pays the best interest.</h2>
-                  <p class="hs-des">Education is not just about going to school and getting a degree. It's about
-                    widening your<br> knowledge and absorbing the truth about life. Knowledge is power.</p>
-                  <div class="site-btn">GET STARTED</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="hs-item" style="background-image: url(/img/hero-slider/2.jpg);">
-          <div class="hs-text">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="hs-subtitle">Award Winning UNIVERSITY</div>
-                  <h2 class="hs-title">An investment in knowledge pays the best interest.</h2>
-                  <p class="hs-des">Education is not just about going to school and getting a degree. It's about
-                    widening your<br> knowledge and absorbing the truth about life. Knowledge is power.</p>
-                  <div class="site-btn">GET STARTED</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </carousel>
-    </client-only>
+<!--    <client-only>-->
+<!--      <carousel v-bind="{-->
+<!--    loop: true,-->
+<!--    nav: false,-->
+<!--    dots: true,-->
+<!--    mouseDrag: false,-->
+<!--    animateOut: 'fadeOut',-->
+<!--    animateIn: 'fadeIn',-->
+<!--    items: 1,-->
+<!--    autoplay: true-->
+<!--  }" class="hero-slider">-->
+    <div>
+      <img src='/img/iaaplicada.png' style='height: 720px; width: 70%; margin: 0 auto; object-fit: contain; display: block;' alt=''>
+    </div>
+<!--        <div class="hs-item" style="background-image: url(/img/hero-slider/2.jpg);">-->
+<!--          <div class="hs-text">-->
+<!--            <div class="container">-->
+<!--              <div class="row">-->
+<!--                <div class="col-lg-8">-->
+<!--                  <div class="hs-subtitle">Award Winning UNIVERSITY</div>-->
+<!--                  <h2 class="hs-title">An investment in knowledge pays the best interest.</h2>-->
+<!--                  <p class="hs-des">Education is not just about going to school and getting a degree. It's about-->
+<!--                    widening your<br> knowledge and absorbing the truth about life. Knowledge is power.</p>-->
+<!--                  <div class="site-btn">GET STARTED</div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </carousel>-->
+<!--    </client-only>-->
   </section>
 
 </template>
