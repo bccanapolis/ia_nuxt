03b96bded767d0da89e12e0a9ffb1e29fc373a5d
devbaraus <me@baraus.dev>
2021-04-13 19:18:08 -0300

    Update pages - A lot of updates

--- pages/pos-graduacao/index.vue
+++ pages/pos-graduacao/index.vue
@@ -514,15 +514,15 @@
                 <h5>Documentos</h5>
                 <p>
                   <a
-                    href='/sites/www.ifg.edu.br/attachments/article/22626/PPC - Pós IA2_Revisao - FINAL.pdf'
-                    rel='alternate'
+                    href='/downloads/PPC%20-%20Pós%20IA2_Revisao%20-%20FINAL.pdf'
+                    download
                   >Projeto Pedagógico do Curso</a
                   >
                 </p>
                 <p>
                   <a
-                    href='/sites/www.ifg.edu.br/attachments/article/22626/ResolucaoAprovacaoCONSUP.pdf'
-                    rel='alternate'
+                    href='/downloads/RESOLUÇÃO%2011_2021%20-%20REI-CONSUP_REITORIA_IFG.pdf'
+                    download
                   >Resolução 55/2021 - Aprovação no CONSUP</a
                   >
                 </p>
