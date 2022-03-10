029cd71d14c8cf95fac0935bfd2cf25905385f22
Bruno de Araujo Alves <me@baraus.dev>
2022-03-09 21:21:10 -0300

    Update edital de seleção

--- components/TabApresentacao.vue
+++ components/TabApresentacao.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <h5>
-      Inteligência Artificial Aplicada - Pós-graduação Lato Sensu
-    </h5>
+    <h5>Inteligência Artificial Aplicada - Pós-graduação Lato Sensu</h5>
     <p>
-      O curso de pós-graduação Lato Sensu ofertado pelo Instituto Federal de Goiás visa formar profissionais
-      com a capacidade de trabalhar e aplicar as tecnologias mais recentes da área de Inteligência
-      Artificial, Aprendizado de Máquina e Recuperação de Informação.
+      O curso de pós-graduação Lato Sensu ofertado pelo Instituto Federal de
+      Goiás visa formar profissionais com a capacidade de trabalhar e aplicar as
+      tecnologias mais recentes da área de Inteligência Artificial, Aprendizado
+      de Máquina e Recuperação de Informação.
     </p>
     <p>
-      Durante o curso, os alunos irão desenvolver competências em Inteligência Artificial, aprender métodos
-      computacionais para gerar padrões e inferências que possam ser aplicados em diversos contextos e
-      negócios. O curso tem ainda por objetivo contribuir para a desenvolvimento socioeconômico e
-      tecnológico da região Centro-Oeste e do Brasil, habilitando novos profissionais para atuarem numa das
-      áreas mais pervasivas da computação, a Inteligência Artificial.
+      Durante o curso, os alunos irão desenvolver competências em Inteligência
+      Artificial, aprender métodos computacionais para gerar padrões e
+      inferências que possam ser aplicados em diversos contextos e negócios. O
+      curso tem ainda por objetivo contribuir para a desenvolvimento
+      socioeconômico e tecnológico da região Centro-Oeste e do Brasil,
+      habilitando novos profissionais para atuarem numa das áreas mais
+      pervasivas da computação, a Inteligência Artificial.
     </p>
-    <p><a href='http://selecao.ifg.edu.br/site/selecao2.php?codigo=1741&situacao=A&tipo=pos'
-          class='font-weight-bolder'>Edital de Seleção</a></p>
+    <p>
+      <a
+        href="http://selecao.ifg.edu.br/site/selecao2.php?codigo=1875&situacao=A&tipo=pos"
+        class="font-weight-bolder"
+        >Edital de Seleção</a
+      >
+    </p>
   </div>
 </template>
 
@@ -25,11 +31,9 @@ export default {
   name: 'TabApresentacao',
   info: {
     name: 'apresentacao',
-    label: 'Apresentação'
-  }
+    label: 'Apresentação',
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
