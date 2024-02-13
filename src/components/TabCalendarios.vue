4fb3433bb696b0b91440dcad456f2d71fbebab49
Bruno de Araújo Alves <me@baraus.dev>
2024-02-12 21:06:03 -0300

    refactor: add new academic calendar

--- src/components/TabCalendarios.vue
+++ src/components/TabCalendarios.vue
@@ -9,6 +9,10 @@
       <a href='/downloads/Calendário2023.pdf' download
       >Calendário Acadêmico 2023</a>
     </p>
+    <p>
+      <a href='/downloads/calendario-EIAA-2024-1.pdf' download
+      >Calendário Acadêmico 2024</a>
+    </p>
   </div>
 </template>
 
