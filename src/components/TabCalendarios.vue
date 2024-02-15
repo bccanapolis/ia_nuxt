eb03081b1ba7ee0f487eebd006f530f4cfb6e951
Bruno de Araujo Alves <devbaraus@gmail.com>
2024-02-15 07:48:05 -0300

    refactor: reordenar calendarios

--- src/components/TabCalendarios.vue
+++ src/components/TabCalendarios.vue
@@ -2,16 +2,16 @@
   <div>
     <h5>Calendários</h5>
     <p>
+      <a href='/downloads/calendario-EIAA-2024-1.pdf' download
+      >Calendário Acadêmico 2024</a>
+    </p>
+     <p>
       <a href='/downloads/CalendarioPos2022.pdf' download
       >Calendário Acadêmico 2022</a>
     </p>
-    <p>
+     <p>
       <a href='/downloads/Calendário2023.pdf' download
       >Calendário Acadêmico 2023</a>
-    </p>
-    <p>
-      <a href='/downloads/calendario-EIAA-2024-1.pdf' download
-      >Calendário Acadêmico 2024</a>
     </p>
   </div>
 </template>
